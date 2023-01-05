f4d6b7e3093277169618e56aa40055e2a4da64d8
RaveX07 <73310649+RaveX07@users.noreply.github.com>
2023-01-05 22:45:11 +0100

    Update settings.py

--- meme_hub/settings.py
+++ meme_hub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eu)1%uu!q9wnys$^t!h*+f8yh_jcy!9oqy5w0f3y=932p4cmd6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
