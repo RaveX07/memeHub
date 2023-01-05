52cac34a1ec3e301a8c6dfbddd9506acb4a9ed2e
RaveX07 <jannis.schroeter@gmx.de>
2023-01-05 23:46:36 +0100

    small changes

--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,33 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponseRedirect, JsonResponse
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.conf import settings
+
 
 from .models import Post
 from .forms import PostForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request=request, template_name="pages/home.html", context={})
 
 def post_create_view(request, *args, **kwargs):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        obj= form.save(commit=False)
-        obj.save()
-        form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        next_url = request.POST.get("next") or None
 
-    return render(request, 'components/form.html', context={"form": form})
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            
+            if next_url != None and url_has_allowed_host_and_scheme(next_url, allowed_hosts=ALLOWED_HOSTS):
+                return redirect(next_url)
+
+            form = PostForm()
+
+        return render(request, 'components/form.html', context={"form": form})
 
 def post_list_view(request, *args, **kwargs):
     qs = Post.objects.all()
